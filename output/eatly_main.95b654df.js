eb13dc5fb69fe301c1605c41415b67051710b8c4
Vladislava Yastremskaya <vladislavayastremskaya@gmail.com>
2026-06-21 13:43:26 +0300

    Initial commit: add landing page project structure

---
+++ output/eatly_main.95b654df.js
@@ -0,0 +1,20 @@
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById("burger").addEventListener("click", function() {
+        document.querySelector(".header").classList.toggle("open");
+    });
+});
+window.addEventListener('keydown', (e)=>{
+    if (e.key === "Escape") document.querySelector(".header").classList.remove("open");
+});
+document.getElementById("menu").addEventListener('click', (event)=>{
+    event._isClickWithInMenu = true;
+});
+document.getElementById("burger").addEventListener('click', (event)=>{
+    event._isClickWithInMenu = true;
+});
+document.body.addEventListener('click', (event)=>{
+    if (event._isClickWithInMenu) return;
+    document.querySelector(".header").classList.remove("open");
+});
+
+//# sourceMappingURL=eatly_main.95b654df.js.map
